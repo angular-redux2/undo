0956b3fb437f538e7cd0b697c56bc62342afc0fe
Garefild <Garefild2@gmail.com>
2023-04-29 04:39:14 +0300

    package: init undo v1.0.0

---
+++ projects/angular-redux2/undo/jest.config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    modulePathIgnorePatterns: [
+        '<rootDir>/dist',
+        '<rootDir>/package.json'
+    ],
+    globals: {
+        'ts-jest': {
+            diagnostics: false
+        }
+    }
+};
